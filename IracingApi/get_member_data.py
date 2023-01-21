399d8a4c1d0952bd5d3907a3ff56519af21b0aa2
Peppe Kerstens <peppekerstens@hotmail.com>
2023-01-21 21:37:46 +0100

    input validation and interactive mode

--- IracingApi/get_member_data.py
+++ IracingApi/get_member_data.py
@@ -21,7 +21,10 @@ def get_iracing_member_data(idc: irDataClient, custid: int):
         "recent": member_recent_races
     }
 
+import numpy
 import pandas as pd 
+import pwinput
+from tabulate import tabulate
 
 def get_member_irating(idc: irDataClient, custid: int):
     iracing_member_data =  get_iracing_member_data(idc, custid)
@@ -32,23 +35,39 @@ def get_member_irating(idc: irDataClient, custid: int):
     mcd = pd.DataFrame.from_dict(member_chart_data['data'])
 
     latest_iRating = mcd.iloc[-1]['value']
+    
+    if latest_iRating > 3750:
+        driver_classification = 'Gold'
+    else:
+        driver_classification = 'Silver'
 
     display_name = member_data['members']
     display_name2 = display_name[0]['display_name']
-    print(f"{custid[0]},{display_name2},{latest_iRating}")
+    #table = [['iRacing Customer ID', 'Driver Name', 'iRating', 'Driver Classifictaion'], 
+                #custid[0], display_name2, latest_iRating, driver_classification]
+    #print(tabulate(table, headers='firstrow', tablefmt='fancy_grid'))
+    print(f"{custid[0]},{display_name2},{latest_iRating},{driver_classification}")
+    #print(tabulate([['custid[0]','display_name2','latest_iRating', 'driver_classification']], headers = 'keys', tablefmt = 'psql'))
 
 import sys
 import csv
 
 if __name__ == '__main__':
    
-    if len(sys.argv) < 4:
-     print('Usage: py get_memberdata.py [username] [password] [cust_id_csv]')
-     sys.exit()
+    if len(sys.argv) == 4:
+        username = sys.argv[1] #first cmdline arg is acnt name
+        password = sys.argv[2] #second cmdline arg is pwd
+        cust_id_csv = sys.argv[3] #third cmdline arg is csv with one table cust_ids (iRacing id)
 
-    username = sys.argv[1] #first cmdline arg is acnt name
-    password = sys.argv[2] #second cmdline arg is pwd
-    cust_id_csv = sys.argv[3] #third cmdline arg is csv with one table cust_ids (iRacing id)
+    else:
+        #raise BaseException('bla')  #does not work; processing is stopped see: https://docs.python.org/3/library/exceptions.html#Exception
+        #print('Error: to few arguments!')
+        #print('Usage: py get_session_data.py [username] [password] [session_id]')
+        print('Going into interactive mode....')
+        username = input("Enter username: ")
+        #password = getpass.getpass('Enter password:') # hard in practise, does not show any input hint
+        password = pwinput.pwinput(prompt='Enter password: ')
+        cust_id_csv = input("Enter Customer ID CSV: ")
     
     idc = irDataClient(username=username, password=password)
 
