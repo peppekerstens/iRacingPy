bdcc51e68a36a78a5d0cd7d40696ac18bf97536e
Peppe Kerstens <peppe.kerstens@ek.co>
2023-01-24 14:02:27 +0100

    bugfix - need only GT3 class for indicator

--- IracingApi/get_session_data.py
+++ IracingApi/get_session_data.py
@@ -136,6 +136,9 @@ if __name__ == '__main__':
     current_track = df_all_tracks['track_id'] == track['track_id']
     df_current_track_detail = df_all_tracks[current_track]
     
+    print()
+    print('Track information')
+    print()
     print(tabulate(df_current_track_detail[['track_name','config_name','track_config_length_km']], headers = 'keys', tablefmt = 'psql'))
 
     #if team_race:
@@ -155,15 +158,17 @@ if __name__ == '__main__':
     #Get the total time driven per team, to calculate percentage per driver later
     df_race_result['avg_lap'] = (df_race_result['average_lap'] / 10000)
     df_race_result['time'] = (df_race_result['avg_lap'] * df_race_result['laps_complete'])
-    total_race_laps = df_race_result['laps_complete'].max()
     
+    print()
+    print('Overall race result')
+    print()
     #df_race_result[['team_id','display_name','avg_lap','laps_complete','time']]
     print(tabulate(df_race_result[['team_id','display_name','avg_lap','laps_complete','time']], headers = 'keys', tablefmt = 'psql'))
 
-    #Get the team results per class - ToDo
+    #Get the team results for the GT3 class
     car_class = df_race_result['car_class_short_name'] == "GT3 Class"
     df_race_result_class = df_race_result[car_class]
-
+    total_race_laps = df_race_result_class['laps_complete'].max()
     #Get the speed an total time driven per driver of each team
     #average_lap = 940294 
     #average_lap = 94.0294 => sec
@@ -174,8 +179,6 @@ if __name__ == '__main__':
     df_driver_result['speed'] = (df_current_track_detail['track_config_length_km'].iloc[-1] / df_driver_result['avg_lap_valid'] * 3600)
     df_driver_result['time'] = (df_driver_result['avg_lap_valid'] * df_driver_result['laps_complete'])
     
-   
-    
     #not there yet....
     #df_driver_result['percentage'] = np.where(df_race_result['team_id'] == df_driver_result['team_id'], 0, df_driver_result['laps_complete'] / df_race_result['laps_complete'])
     #throws ValueError: Can only compare identically-labeled Series objects
@@ -183,7 +186,10 @@ if __name__ == '__main__':
     df_driver_result['percentage'] = round(df_driver_result['laps_complete'] / total_race_laps * 100,0)
     #df_driver_result['percentage'] = round(df_driver_result['laps_complete'] / get_total_laps(df_driver_result, df_driver_result['team_id'].iloc[0]) * 100,0)
     
-    #df_driver_result[['team_id','cust_id','display_name', 'avg_lap','laps_complete','speed','time', 'percentage']] 
+    #df_driver_result[['team_id','cust_id','display_name', 'avg_lap','laps_complete','speed','time', 'percentage']]
+    print()
+    print('GT3 Class driver result')
+    print()
     print(tabulate(df_driver_result[['team_id','cust_id','display_name', 'avg_lap','avg_lap_valid','laps_complete','speed','time', 'percentage']], headers = 'keys', tablefmt = 'psql'))
 
 
