b807605dd6883feacbd8490f12df0be59c0231b5
Peppe Kerstens <peppekerstens@hotmail.com>
2023-01-29 21:47:38 +0100

    alpha working

--- IracingApi/update_gt3_driver_indicator.py
+++ IracingApi/update_gt3_driver_indicator.py
@@ -25,19 +25,23 @@ def import_csv(path: str) -> dict:
 # get previous indicator
 # must either be result of previous race -or if first race- result from iRating analysis
 try:
-    df_previous_state = pd.read_csv(driver_indicator_file)
+    df_indicator = pd.read_csv(driver_indicator_file)
 except:
-    #scenario first race
+    #scenario first race - REWRITE: SHOULD ALWAYS LOAD TO ADD DRIVERS!!!! CALL IT DF_MEMBERS /DF_REF
     df_indicator = pd.DataFrame(columns=['cust_id','display_name','race_count','old_classification','total_time','avg_speed','percentage','new_classification'])
     #add values from the member_data file as first reference
     #member_data = import_csv(member_data_file)
     df_member_data = pd.read_csv(member_data_file)
     for index, df_row in df_member_data.iterrows():
+        classification = df_row['driver_classification']
+        #print(df_row['driver_classification'])
         if df_row['driver_classification'] == 'Not allowed':
+            #print(df_row['driver_classification'])
             classification = 'Gold'
-        else:
-            classification = df_row['driver_classification']
-        df_indicator.loc[index] = [df_row['cust_id'] ,df_row['display_name'],0,classification,0,0,0,'None']
+        #else:
+        #    classification = df_row['driver_classification']
+        #df_indicator.loc[index] = [df_row['cust_id'] ,df_row['display_name'],0,classification,0,0,0,'None']
+        df_indicator.loc[index] = [df_row['cust_id'] ,df_row['display_name'],0,classification,0,0,0,classification]
 
 print(tabulate(df_indicator, headers = 'keys', tablefmt = 'psql'))
 
@@ -65,6 +69,7 @@ print(tabulate(df_latest_session, headers = 'keys', tablefmt = 'psql'))
 
 #move the new_classification to old_classification
 df_indicator['old_classification'] = df_indicator['new_classification']
+#
 
 #build intermediate dataframe based on current results
 df_pec_driver_info = pd.DataFrame(columns=['cust_id','display_name','race_count','old_classification','total_time','avg_speed','percentage','new_classification'])
@@ -74,7 +79,7 @@ for index, df_row in df_latest_session.iterrows():
     if df_indicator_driver.empty:
         print(f"WARNING: could not find indicator information for driver {df_row['display_name']} ({df_row['cust_id']}) skipping!")
     else:
-        print(tabulate(df_indicator_driver, headers = 'keys', tablefmt = 'psql'))
+        #print(tabulate(df_indicator_driver, headers = 'keys', tablefmt = 'psql'))
         old_classification = df_indicator_driver.iloc[0]['old_classification']
         cust_id = df_row['cust_id'] 
         display_name = df_row['display_name']
@@ -96,24 +101,16 @@ df_pec_driver_info_sorted = df_pec_driver_info.sort_values(by='avg_speed', ascen
 total_drivers = len(df_pec_driver_info_sorted)
 gold_drivers = df_pec_driver_info_sorted['old_classification'] == 'Gold'
 df_indicator_gold_drivers = df_pec_driver_info_sorted[gold_drivers]
-gold_driver_count = len(df_pec_driver_info_sorted)
+gold_driver_count = len(df_indicator_gold_drivers)
 silver_driver_count = total_drivers - gold_driver_count
-gold_driver_deadzone = round(gold_driver_count * 0,3)
-silver_driver_deadzone = round(silver_driver_count * 0,3)
-print(silver_driver_deadzone)
-print(gold_driver_deadzone)
+gold_driver_deadzone = round(gold_driver_count * 0.3)
+silver_driver_deadzone = round(silver_driver_count * 0.3)
 if silver_driver_deadzone > 0 and gold_driver_deadzone >0:
     silver_deadzone_limit = gold_driver_count + silver_driver_deadzone
     gold_deadzone_limit = gold_driver_count - gold_driver_deadzone
     silver_deadzone_speed = df_pec_driver_info.iloc[silver_deadzone_limit]['avg_speed']
     gold_deadzone_speed = df_pec_driver_info.iloc[gold_deadzone_limit]['avg_speed']
-
-    print('deadzone speed')
-    print(silver_deadzone_speed)
-    print(gold_deadzone_speed)
-
     #review created list for new_classification
-    
     for index, df_row in df_pec_driver_info_sorted.iterrows():
         if df_row['avg_speed'] > gold_deadzone_speed:
             new_classification = 'Gold'
@@ -124,7 +121,7 @@ if silver_driver_deadzone > 0 and gold_driver_deadzone >0:
         if new_classification == 'Gold':
             driver_indicator = df_indicator['cust_id'] == df_row['cust_id'] 
             df_indicator_driver = df_indicator[driver_indicator]
-            percentage = (df_indicator_driver['race_count'] * df_indicator_driver['percentage'] + df_row['percentage']) / df_row['race_count']
+            percentage = (df_indicator_driver.iloc[0]['race_count'] * df_indicator_driver.iloc[0]['percentage'] + df_row['percentage']) / df_row['race_count']
         else:
             percentage =  df_indicator_driver['percentage']
         df_new_indicator.loc[index] = [df_row['cust_id'] ,df_row['display_name'],df_row['race_count'],df_row['old_classification'],df_row['total_time'],df_row['avg_speed'],percentage,new_classification]
