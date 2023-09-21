3a725a16173a5e2808d3f92c05d3904be862e496
Peppe Kerstens <peppekerstens@hotmail.com>
2023-09-21 08:30:01 +0200

    driver_indicator

--- update_driver_indicator_s4.py
+++ update_driver_indicator_s4.py
@@ -7,6 +7,27 @@ from tabulate import tabulate
 import glob
 
 driver_indicator_columns = ['team_id','team_display_name','cust_id','display_name','race_count','old_classification','total_time','avg_speed','percentage','new_classification','deadzone','reclassified','driven','gold_race_count','gold_total_drive_time','gold_total_sesion_time','gold_total_percentage','index','total_index','total_session_time']
+driver_indicator_columns = ['team_id','team_display_name','cust_id','display_name','classification','percentage','total_time','total_session_time']
+
+def get_total_session_time(result: json, ): #assumes all contenders (teams of drivers) within a session (race, qually etc), eg all memebers under ['session_results']
+    total_time = 0
+    for contender in result:
+        if contender['finish_position'] == 0:
+            contender_time = contender['laps_complete'] * contender['average_lap']
+            total_time += contender_time
+    total_time = total_time / 10000
+    return total_time
+
+def get_team_driver_results(result: json, ):
+    new_list = []
+    for team_results in result:
+        team_display_name = team_results["display_name"]
+        #if  session_results['simsession_type_name'] == type:
+        for driver in team_results['driver_results']:
+            driver['team_display_name'] = team_display_name
+            new_list.append(driver)
+    return new_list
+
 
 def generate_member_data_driver_indicator(df_member_data: pd, cust_id: int) -> pd:
     #scenario first time driver. use downloaded member_data (which should contain classification based on iRating) as a reference
@@ -21,19 +42,19 @@ def generate_member_data_driver_indicator(df_member_data: pd, cust_id: int) -> p
             #print(df_row['driver_classification'])
             classification = 'Gold'
         #                        ['team_id','cust_id','display_name','race_count','old_classification','total_time','avg_speed','percentage','new_classification','deadzone','reclassified','driven','gold_race_count','gold_total_drive_time','gold_total_sesion_time','index','total_index']
-        df_indicator.loc[0] = [0,'',df_driver.iloc[0]['cust_id'] ,df_driver.iloc[0]['display_name'],0,classification,0,0,0,classification,False,False,False,0,0,0,0,0,0,0]
+        df_indicator.loc[0] = [0,'',df_driver.iloc[0]['cust_id'] ,df_driver.iloc[0]['display_name'],classification,0,0,0]
     return df_indicator
 
 
 def update_driver_info(df_latest_session: pd,df_member_data: pd) -> pd:
-    #build intermediate dataframe based on current results
+    #get all teams 
+
+    #build dataframe based on current results
     df_driver_info = pd.DataFrame(columns=driver_indicator_columns)
     for index, df_row in df_latest_session.iterrows():
         cust_id = df_row['cust_id']
         df_indicator_driver = generate_member_data_driver_indicator(df_member_data,cust_id)
         if not df_indicator_driver.empty:
-            #print(tabulate(df_indicator_driver, headers = 'keys', tablefmt = 'psql'))
-            old_classification = df_indicator_driver.iloc[0]['old_classification']
             try:
                 team_id = df_row['team_id']
             except:
@@ -41,12 +62,9 @@ def update_driver_info(df_latest_session: pd,df_member_data: pd) -> pd:
             cust_id = df_row['cust_id']
             display_name = df_row['display_name']
             team_display_name = df_row['team_display_name']
-            new_classification = df_indicator_driver.iloc[0]['new_classification']
-            race_count = df_indicator_driver.iloc[0]['race_count'] + 1 # https://stackoverflow.com/questions/16729574/how-can-i-get-a-value-from-a-cell-of-a-dataframe
-            avg_speed = df_row['speed_valid']
-            total_time = df_row['time_valid']
+            classification = df_indicator_driver.iloc[0]['classification']
             total_session_time = df_row['total_session_time']
-            
+            total_time = df_row['time_valid']
             percentage = round(total_time/total_session_time * 100,0)
             #print(f"cust_id: {df_row['cust_id']}")
             #print(f"total_time: {total_time}")
@@ -54,96 +72,13 @@ def update_driver_info(df_latest_session: pd,df_member_data: pd) -> pd:
             #print(f"percentage: {percentage}")
             #total_time = round(df_indicator_driver.iloc[0]['total_time'] + df_row['time_valid'],2)
             # ['team_id','team_display_name','cust_id','display_name','race_count','old_classification','total_time','avg_speed','percentage','new_classification','deadzone','reclassified','driven','gold_race_count','gold_total_drive_time','gold_total_sesion_time','gold_total_percentage','index','total_index','total_session_time']
-            df_driver_info.loc[index] = [team_id,team_display_name,cust_id,display_name,race_count,old_classification,total_time,avg_speed,percentage,new_classification,False,False,False,0,0,0,0,0,0,total_session_time]
+            df_driver_info.loc[index] = [team_id,team_display_name,cust_id,display_name,classification,percentage,total_time,total_session_time]
+            #driver_indicator_columns = ['team_id','team_display_name','cust_id','display_name','race_count','old_classification','total_time','avg_speed','percentage','new_classification','deadzone','reclassified','driven','gold_race_count','gold_total_drive_time','gold_total_sesion_time','gold_total_percentage','index','total_index','total_session_time']
+            #driver_indicator_columns = ['team_id','team_display_name','cust_id','display_name','classification','percentage','total_time','total_session_time']
+
         if df_indicator_driver.empty:    
             print(f"WARNING: could not find indicator information for driver {df_row['display_name']} ({df_row['cust_id']}) skipping!")
     return df_driver_info
-
-def update_driver_indicator(df_driver_info: pd, df_indicator: pd, df_member_data: pd ) -> pd:
-    df_new_indicator = pd.DataFrame(columns=driver_indicator_columns)
-    #how large is is the deadzone?
-    silver_deadzone_speed = 0
-    gold_deadzone_speed = 0
-    df_driver_info_sorted = df_driver_info.sort_values(by='avg_speed', ascending=False)
-    total_drivers = len(df_driver_info_sorted)
-    gold_drivers = df_driver_info_sorted['old_classification'] == 'Gold'
-    df_driver_info_gold_drivers = df_driver_info_sorted[gold_drivers]
-    gold_driver_count = len(df_driver_info_gold_drivers)
-    silver_driver_count = total_drivers - gold_driver_count
-    gold_driver_deadzone = round(gold_driver_count * 0.3)
-    silver_driver_deadzone = round(silver_driver_count * 0.3)
-    if silver_driver_deadzone > 0 and gold_driver_deadzone > 0:
-        silver_deadzone_limit = gold_driver_count + silver_driver_deadzone
-        gold_deadzone_limit = gold_driver_count - gold_driver_deadzone
-        silver_deadzone_speed = df_driver_info_sorted.iloc[silver_deadzone_limit]['avg_speed']
-        gold_deadzone_speed = df_driver_info_sorted.iloc[gold_deadzone_limit]['avg_speed']
-        #print(f"total_drivers: {total_drivers}")
-        #print(f"gold_driver_count: {gold_driver_count}")
-        #print(f"silver_driver_count: {silver_driver_count}")
-        #print(f"silver_deadzone_limit: {silver_deadzone_limit}")
-        #print(f"gold_deadzone_limit: {gold_deadzone_limit}")
-        #print(f"silver_deadzone_speed: {silver_deadzone_speed}")
-        #print(f"gold_deadzone_speed: {gold_deadzone_speed}")
-
-        #review created list for new_classification
-        for index, df_row in df_driver_info_sorted.iterrows():
-            #get the old indicator info
-            driver = df_indicator['cust_id'] == df_row['cust_id']
-            df_indicator_driver = df_indicator[driver]
-
-            if df_indicator_driver.empty:
-                gold_race_count = 0
-                gold_total_drive_time = 0
-                gold_total_sesion_time = 0
-                gold_total_percentage = 0
-
-            if not df_indicator_driver.empty:
-                gold_race_count = df_indicator_driver.iloc[0]['gold_race_count']
-                gold_total_drive_time = df_indicator_driver.iloc[0]['gold_total_drive_time']
-                gold_total_sesion_time = df_indicator_driver.iloc[0]['gold_total_sesion_time']
-                gold_total_percentage = df_indicator_driver.iloc[0]['gold_total_percentage']
-
-            deadzone = False
-            if df_row['avg_speed'] > gold_deadzone_speed:
-                new_classification = 'Gold'
-            if df_row['avg_speed'] < silver_deadzone_speed:
-                new_classification = 'Silver'
-            if df_row['avg_speed'] <= gold_deadzone_speed and df_row['avg_speed'] >= silver_deadzone_speed:
-                new_classification = df_row['old_classification']
-                deadzone = True
-            reclassified = False   
-            if new_classification != df_row['old_classification']:
-                reclassified = True
-            #df_indicator_driver = get_df_indicator_driver(df_indicator,df_member_data, df_row['cust_id'])
-
-            if new_classification == 'Gold':
-                #'gold_race_count','gold_total_drive_time','gold_total_sesion_time',gold_total_percentage, 'index','total_index'
-                gold_race_count += 1
-                gold_total_drive_time = round(gold_total_drive_time + df_row['total_time'],2)
-                gold_total_sesion_time = round(gold_total_sesion_time + df_row['total_session_time'],2)
-                gold_total_percentage = round(gold_total_drive_time/gold_total_sesion_time * 100,0)
-                #gold_total_time = round(df_indicator_driver.iloc[0]['total_time'] + df_row['time_valid'],2)
-
-                #print(f"hoi {df_row['display_name']} {df_indicator_driver.iloc[0]['race_count']} {df_indicator_driver.iloc[0]['percentage']} {df_row['percentage']} {df_row['race_count']}")
-                #percentage = (df_indicator_driver.iloc[0]['race_count'] * df_indicator_driver.iloc[0]['percentage'] + df_row['percentage']) / df_row['race_count']
-            #else:
-            #    percentage =  df_indicator_driver.iloc[0]['percentage']
-                #print(f"hoi { df_row['display_name']} {df_indicator_driver.iloc[0]['percentage']}")
-            #percentage =  df_indicator_driver.iloc[0]['percentage']
-            driven = True
-                                                                                                                                                                        #'gold_race_count','gold_total_drive_time','gold_total_sesion_time',gold_total_percentage, 'index','total_index'
-            df_new_indicator.loc[index] = [df_row['team_id'],df_row['team_display_name'],df_row['cust_id'] ,df_row['display_name'],df_row['race_count'],df_row['old_classification'],df_row['total_time'],df_row['avg_speed'],df_row['percentage'],new_classification,deadzone,reclassified,driven,gold_race_count,gold_total_drive_time,gold_total_sesion_time,gold_total_percentage,0,0,df_row['total_session_time']]
-    
-        #now combine any old values which may not have been updated with the new Dataframe
-        df_not_in_common = df_indicator.loc[~df_indicator['cust_id'].isin(df_new_indicator['cust_id'])]
-        df_not_in_common['driven'] = False
-        frames = [df_not_in_common, df_new_indicator]
-        result = pd.concat(frames)
-    else:
-        print(f"WARNING: detected deadzone too small to use!")
-        return df_indicator
-    return result
-
 
 if __name__ == '__main__': #only execute when called as script, skipped when loaded as module
     #https://stackoverflow.com/questions/40001892/reading-named-command-arguments
@@ -177,12 +112,16 @@ if __name__ == '__main__': #only execute when called as script, skipped when loa
     #print(tabulate(df_latest_session, headers = 'keys', tablefmt = 'psql'))
 
     #we only need to process the GT3 car_class
-    car_classes = ['GT3 Class']
+    car_classes = ['IMSA23']
     for car_class in car_classes:
         latest_session_class = df_latest_session["car_class_short_name"] == car_class
         df_latest_session_class = df_latest_session[latest_session_class]
 
         df_driver_info = update_driver_info(df_latest_session_class,df_member_data)
+
+        driver_indicator_file = './indicators/driver_indicator.csv'
+        df_driver_info.to_csv(driver_indicator_file,index=False)
+        """
         print(tabulate(df_driver_info))
         df_new_indicator = update_driver_indicator(df_driver_info, df_member_data)
         print(tabulate(df_new_indicator[['cust_id','display_name','race_count','old_classification','total_time','avg_speed','percentage','new_classification','deadzone','reclassified']], headers = 'keys', tablefmt = 'psql'))
@@ -190,3 +129,4 @@ if __name__ == '__main__': #only execute when called as script, skipped when loa
         #parser.add_argument("--driver_indicator", help="location of the driver indicator file. if only name is supplied, script location is used",  default='./indicators/driver_indicator.csv', type=str)
         driver_indicator_file = './indicators/driver_indicator.csv'
         df_new_indicator.to_csv(driver_indicator_file,index=False)
+        """
