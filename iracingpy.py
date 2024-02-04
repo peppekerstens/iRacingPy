a73d311354dcacf2db8f744dcbd66814950275f0
Peppe Kerstens <peppekerstens@hotmail.com>
2024-02-04 22:31:46 +0100

    cleanup before s5

--- iracingpy.py
+++ iracingpy.py
@@ -1,8 +1,11 @@
+from  iracingdataapi.client import irDataClient
 import yaml
+import argparse
 import os
 
 #maybe use this?
 #https://able.bio/rhett/how-to-set-and-get-environment-variables-in-python--274rgt5
+#https://realpython.com/python-yaml/
 
 config_file = 'iracingpy.config.yaml'
 
@@ -20,22 +23,31 @@ def set_config(config_file: str): #improve to path?
             config, stream=f, default_flow_style=False, sort_keys=False
         )
 
+def get_member_data(idc: irDataClient, cust_id):
+    member_data = None
+    try:
+        member_data = idc.member(cust_id=cust_id)
+        #df_member_data = pd.DataFrame.from_dict(member_data['members'])
+        #return df_member_data
+    except:
+        return member_data
+    return member_data 
 
 if __name__ == '__main__': #only execute when called as script, skipped when loaded as module
-    #https://stackoverflow.com/questions/40001892/reading-named-command-arguments
-    #https://stackoverflow.com/questions/15301147/python-argparse-default-value-or-specified-value
-    #expand later to use a config file instead of defaults
-    parser=argparse.ArgumentParser()
-    parser.add_argument("--member_data", help="location of member data file. if only name is supplied, script location is used",  default='member_data.csv', type=str)
-    parser.add_argument("--driver_indicator", help="location of the driver indicator file. if only name is supplied, script location is used",  default='pec_s3_driver_indicator.csv', type=str)
-    parser.add_argument("--session", help="location of the session file to process. if only name is supplied, script location is used")
+    config = get_config(config_file)
+    print(f"Dict format: {config['league_id']}")
+
+    #league_information_path = config['base_path']+config['league_path']+config['league_information_file'] + ".json"
+
+    idc = irDataClient(username=username, password=password)
+
+    # Get the list of drivers in the league
+    league_information = idc.league_get(league_id)
+    league_roster = league_information['roster']
+    members = [] 
+    for member in league_roster:
+        member_data = get_member_data(idc, member['cust_id'])
+        if member_data != None:
+            members += member_data['members']
     
-    args=parser.parse_args()
-    #print(f"Args: {args}\nCommand Line: {sys.argv}\nfoo: {args.foo}")
-    #print(f"Dict format: {vars(args)}")
-
-    member_data_file = args.member_data #up-to-date info on members irating and validation (only needed for first race)
-    #cust_id,display_name,latest_iRating,driver_classification,driver_qualification
-    driver_indicator_file = args.driver_indicator #server both as reference data set from previous race(s) as well as file to save latest status to after processing
-    latest_session_file = args.session #results from current race. maybe an agrument for this script? currently being detected as latest file within directory with name session_*.csv
-
+    
