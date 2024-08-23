1067ef85094460bf9daa1a0edb9bdfc96e81fc86
Peppe Kerstens <peppekerstens@hotmail.com>
2024-08-23 12:45:08 +0200

    improved parameter parsing and flow

--- get_league_information.py
+++ get_league_information.py
@@ -6,63 +6,28 @@ import json
 from iracingdataapi.client import irDataClient
 import pandas as pd
 from tqdm import tqdm #https://github.com/tqdm/tqdm/#readme
+import argparse
 
-if __name__ == '__main__': 
-    league_information_file = 'league_information'
-    league_roster_file = 'league_roster'
-    league_pending_requests_file = 'league_pending_requests'
-    league_seasons_file = 'league_seasons'
-    league_season_sessions_file = 'league_season_sessions'
-    league_id = 5606
-    #season_id = 83043 #s2
-    #season_id = 89417 #s3
-    season_id = 95271 #s4
+def dump_json(information, filename: 'str') -> None:
+    with open(filename + ".json", "w") as write_file:
+        json.dump(information, write_file, indent=4, sort_keys=True)
 
-    print('Going into interactive mode....')
-    username = input("Enter username: ")
-    password = pwinput.pwinput(prompt='Enter password: ')
-
-    idc = irDataClient(username=username, password=password)
-
-    # Get the list of drivers in the league
-    league_information = idc.league_get(league_id)
-    with open(league_information_file + ".json", "w") as write_file:
-        json.dump(league_information, write_file, indent=4, sort_keys=True)
-
+def dump_csv_league_pending_requests(league_information, filename: 'str') -> None:
     df_league_pending_requests = pd.json_normalize(league_information['pending_requests'])
     #print(tabulate(df_league_pending_requests[['cust_id','display_name','initiated','revoked']], headers = 'keys', tablefmt = 'psql'))
-    df_league_pending_requests.to_csv(league_pending_requests_file + ".csv",index=False,columns=['cust_id','display_name','initiated','revoked'])  
+    df_league_pending_requests.to_csv(filename + ".csv",index=False,columns=['cust_id','display_name','initiated','revoked'])  
 
+def dump_csv_league_roster(league_information, filename: 'str') -> None:
     df_league_roster = pd.json_normalize(league_information['roster'])
     #print(tabulate(df_league_roster[['cust_id','display_name','league_member_since','admin']], headers = 'keys', tablefmt = 'psql'))
-    df_league_roster.to_csv(league_roster_file + ".csv",index=False,columns=['cust_id','display_name','league_member_since','admin'])  
+    df_league_roster.to_csv(filename + ".csv",index=False,columns=['cust_id','display_name','league_member_since','admin'])  
 
-
-    """
-    session_id': 186249222,
-    'private_session_id': 2993437,
-    'session_id': 186249222,
-    'qualify_length': 60, 'race_laps': 0, 'race_length': 600,
-    track_name': 'Road America', 'config_name': 'Full Course'
-    'time_limit': 300
-    """
-
-    league_seasons = idc.league_seasons(league_id)
-    with open(league_seasons_file + ".json", "w") as write_file:
-        json.dump(league_seasons, write_file, indent=4, sort_keys=True)
+def dump_csv_league_seasons_sessions(league_seasons, filename: 'str') -> None:
     df_league_seasons = pd.DataFrame.from_dict(league_seasons['seasons'])
     #print(tabulate(df_league_seasons[['session_id','time_limit','qualify_length','race_length']], headers = 'keys', tablefmt = 'psql'))
-    df_league_seasons.to_csv(league_season_sessions_file + ".csv",index=False)  
+    df_league_seasons.to_csv(filename + ".csv",index=False)  
 
-
-    league_season_sessions = idc.league_season_sessions(league_id, season_id)
-    with open(league_season_sessions_file + ".json", "w") as write_file:
-        json.dump(league_season_sessions, write_file, indent=4, sort_keys=True)
-    df_league_season_sessions = pd.DataFrame.from_dict(league_season_sessions['sessions'])
-    #print(tabulate(df_league_season_sessions[['session_id','time_limit','qualify_length','race_length']], headers = 'keys', tablefmt = 'psql'))
-    df_league_season_sessions.to_csv(league_season_sessions_file + ".csv",index=False,columns=['session_id','time_limit','qualify_length','race_length'])  
-
-
+""""
     def extract_cust_id(data):
         cust_ids = []
         if isinstance(data, dict):
@@ -83,7 +48,68 @@ if __name__ == '__main__':
         writer.writerow(['cust_id'])
         for cust_id in cust_ids:
             writer.writerow([cust_id])
+"""
 
 
+if __name__ == '__main__': 
+    league_information_file = 'league_information'
+    league_roster_file = 'league_roster'
+    league_pending_requests_file = 'league_pending_requests'
+    league_seasons_file = 'league_seasons'
+    league_season_sessions_file = 'league_season_sessions'
 
+    #https://stackoverflow.com/questions/40001892/reading-named-command-arguments
+    #https://stackoverflow.com/questions/15301147/python-argparse-default-value-or-specified-value
+    #expand later to use a config file instead of defaults
+    parser=argparse.ArgumentParser()
+    parser.add_argument("--username", help="", default='', type=str)
+    parser.add_argument("--password", help="", default='', type=str)
+    parser.add_argument("--league_id", help="", default=5606, type=int) #PEC league
+    parser.add_argument("--season_id", help="", default=110644, type=int) #season 6
+    parser.add_argument("--csv", help="", type=bool)
+    parser.add_argument("--json", help="", default=True, type=bool)
 
+    args=parser.parse_args()
+    #print(f"Args: {args}\nCommand Line: {sys.argv}\nfoo: {args.foo}")
+    #print(f"Dict format: {vars(args)}")
+    
+    username = args.username
+    password = args.password
+    league_id = args.league_id
+    season_id = args.season_id
+    jsondump = args.json
+    csvdump = args.csv
+
+    if username == "":
+        username = input("Enter username: ")
+
+    if password == "":
+        password = pwinput.pwinput(prompt='Enter password: ')
+
+    if league_id == "":
+        league_id = input("Enter league_id: ")
+
+    if season_id == "":
+        season_id = input("Enter season_id: ")
+
+    idc = irDataClient(username=username, password=password)
+
+    # Get generic league information
+    league_information = idc.league_get(league_id)
+
+    # Get all league seasons
+    league_seasons = idc.league_seasons(league_id)
+
+    # Get all league seasons sessions
+    league_season_sessions = idc.league_season_sessions(league_id, season_id)
+    
+
+    if jsondump == True:
+        dump_json(league_information, league_information_file)
+        dump_json(league_seasons, league_information_file)
+        dump_json(league_season_sessions, league_season_sessions_file)
+
+    if csvdump == True:
+        dump_csv_league_pending_requests(league_information, league_pending_requests_file)
+        dump_csv_league_roster(league_information, league_roster_file)
+        dump_csv_league_seasons_sessions(league_season_sessions, league_season_sessions_file)
