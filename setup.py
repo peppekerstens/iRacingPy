dff1723aa95067b4f83b9afcbb1f4bec4ac10358
Peppe Kerstens <peppekerstens@hotmail.com>
2024-09-10 21:47:11 +0200

    main setup

---
+++ setup.py
@@ -0,0 +1,19 @@
+# iracing credentials vragen
+
+# league id vragen
+
+# actief season achterhalen, lukt dat niet, dan seizoenen tonen en vragen welke
+
+# welke actie is gewenst?
+1. leden ophalen
+2. resultaten ophalen
+3. timedelta achterhalen
+4. driver indicator ophalen
+5. 
+
+# informatie opslaan
+1. als json
+2. als csv
+3. omzetten naar/posten als google document
+4. presenteer als web pagina
+
